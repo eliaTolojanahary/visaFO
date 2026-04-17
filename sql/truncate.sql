6dca43340a68f13ae319b8e1b42d16bdb584f38e
Jemima-Harisoa <jemimarazafimandimby@gmail.com>
2026-04-17 15:37:37 +0300

    chore: script truncate table

---
+++ sql/truncate.sql
@@ -0,0 +1,12 @@
+
+DROP TABLE IF EXISTS demande_piece;
+DROP TABLE IF EXISTS demande;
+DROP TABLE IF EXISTS passeport;
+DROP TABLE IF EXISTS piece_justificative_ref;
+DROP TABLE IF EXISTS demandeur;
+DROP TABLE IF EXISTS statut_demande;
+DROP TABLE IF EXISTS type_titre;
+DROP TABLE IF EXISTS type_demande;
+DROP TABLE IF EXISTS nationalite;
+DROP TABLE IF EXISTS situation_famille;
+
