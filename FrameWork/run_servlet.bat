563526d6ff5d5edd909cd4c432d76d3f72306d46
Jemima-Harisoa <jemimarazafimandimby@gmail.com>
2026-04-21 14:11:08 +0300

    perfs: Refactor run_servlet.bat to improve Java compilation process by organizing source files into layers and enhancing error handling

--- FrameWork/run_servlet.bat
+++ FrameWork/run_servlet.bat
@@ -5,6 +5,8 @@ set "BUILD_PATH=%PROJECT_PATH%build"
 set "WEBAPP_PATH=%PROJECT_PATH%src\main\webapp"
 set "CATALINA_HOME=C:\xampp\tomcat"
 set "LIB_PATH=%PROJECT_PATH%lib"
+set "APP_WAR=reservation.war"
+set "COMMON_CLASSPATH=%BUILD_PATH%\WEB-INF\classes;%CATALINA_HOME%\lib\servlet-api.jar;%LIB_PATH%\*"
 
 rem Vérifier si le dossier "build" existe et le supprimer
 if exist "%BUILD_PATH%" (
@@ -26,17 +28,46 @@ mkdir "%BUILD_PATH%"
 mkdir "%BUILD_PATH%\WEB-INF"
 mkdir "%BUILD_PATH%\WEB-INF\classes"
 
-rem Compilation des fichiers .java et sortie dans le répertoire classes
+rem Compilation des fichiers .java par couches de dépendances
 echo Compilation des fichiers Java...
 
-rem Générer la liste des sources dans un fichier temporaire
 set "SOURCES=%TEMP%\sources.txt"
 if exist "%SOURCES%" del /f /q "%SOURCES%"
-for /R "%PROJECT_PATH%src\main\java" %%F in (*.java) do @echo %%~fF>>"%SOURCES%"
 
-rem Compiler en utilisant la liste de sources (gère les sous-dossiers et packages)
-rem Ajout de -parameters pour préserver les noms des paramètres (requis pour le binding automatique)
-javac -parameters -d "%BUILD_PATH%\WEB-INF\classes" -classpath "%CATALINA_HOME%\lib\servlet-api.jar;%LIB_PATH%\*" @"%SOURCES%"
+echo   - couche 1: annotation, util, modelview
+for /R "%PROJECT_PATH%src\main\java\annotation" %%F in (*.java) do @echo %%~fF>>"%SOURCES%"
+for /R "%PROJECT_PATH%src\main\java\util" %%F in (*.java) do @echo %%~fF>>"%SOURCES%"
+for /R "%PROJECT_PATH%src\main\java\modelview" %%F in (*.java) do @echo %%~fF>>"%SOURCES%"
+javac -parameters -d "%BUILD_PATH%\WEB-INF\classes" -classpath "%COMMON_CLASSPATH%" @"%SOURCES%"
+if errorlevel 1 goto :compile_failed
+
+echo   - couche 2: models
+if exist "%SOURCES%" del /f /q "%SOURCES%"
+for /R "%PROJECT_PATH%src\main\java\models" %%F in (*.java) do @echo %%~fF>>"%SOURCES%"
+javac -parameters -d "%BUILD_PATH%\WEB-INF\classes" -classpath "%COMMON_CLASSPATH%" @"%SOURCES%"
+if errorlevel 1 goto :compile_failed
+
+echo   - couche 3: dao et repo
+if exist "%SOURCES%" del /f /q "%SOURCES%"
+for /R "%PROJECT_PATH%src\main\java\dao" %%F in (*.java) do @echo %%~fF>>"%SOURCES%"
+for /R "%PROJECT_PATH%src\main\java\repo" %%F in (*.java) do @echo %%~fF>>"%SOURCES%"
+javac -parameters -d "%BUILD_PATH%\WEB-INF\classes" -classpath "%COMMON_CLASSPATH%" @"%SOURCES%"
+if errorlevel 1 goto :compile_failed
+
+echo   - couche 4: services
+if exist "%SOURCES%" del /f /q "%SOURCES%"
+for /R "%PROJECT_PATH%src\main\java\services" %%F in (*.java) do @echo %%~fF>>"%SOURCES%"
+javac -parameters -d "%BUILD_PATH%\WEB-INF\classes" -classpath "%COMMON_CLASSPATH%" @"%SOURCES%"
+if errorlevel 1 goto :compile_failed
+
+echo   - couche 5: controllers, servlet, scan, main
+if exist "%SOURCES%" del /f /q "%SOURCES%"
+for /R "%PROJECT_PATH%src\main\java\controllers" %%F in (*.java) do @echo %%~fF>>"%SOURCES%"
+for /R "%PROJECT_PATH%src\main\java\servlet" %%F in (*.java) do @echo %%~fF>>"%SOURCES%"
+for /R "%PROJECT_PATH%src\main\java\scan" %%F in (*.java) do @echo %%~fF>>"%SOURCES%"
+for /R "%PROJECT_PATH%src\main\java\main" %%F in (*.java) do @echo %%~fF>>"%SOURCES%"
+javac -parameters -d "%BUILD_PATH%\WEB-INF\classes" -classpath "%COMMON_CLASSPATH%" @"%SOURCES%"
+if errorlevel 1 goto :compile_failed
 
 rem Copier les bibliothèques (JARs) dans WEB-INF\lib
 echo Copie des bibliothèques JAR...
@@ -64,3 +95,10 @@ move /Y "%BUILD_PATH%\reservation.war" "%CATALINA_HOME%\webapps\"
 
 echo Projet Servlet déployé et Tomcat prêt à démarrer.
 pause
+goto :eof
+
+:compile_failed
+echo.
+echo Compilation interrompue. Le WAR n'a pas été généré.
+pause
+exit /b 1
