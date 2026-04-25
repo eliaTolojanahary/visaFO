c8b487035bf82b0a94185a348c3fffd4608bdab2
Jemima-Harisoa <jemimarazafimandimby@gmail.com>
2026-04-25 21:55:52 +0300

    fix: add column existence checks in DemandeRepository and update start.bat script for clarity

--- start.bat
+++ start.bat
@@ -13,13 +13,17 @@ if "%DB%"=="" (
     exit /b 1
 )
 
-echo [1/2] Creation des tables...
+echo [1/3] Creation des tables...
 psql -U %User% -d %DB% -f sql/table.sql
 if %ERRORLEVEL% neq 0 ( echo ERREUR etape 2 & exit /b 1 )
 
-echo [2/2] Insertion des donnees de test...
-psql -U %User% -d %DB% -f sql/donnees_mini_test.sql
+echo [2/3] Application des evolutions Sprint 2...
+psql -U %User% -d %DB% -f sql/alter_sprint2.sql
 if %ERRORLEVEL% neq 0 ( echo ERREUR etape 3 & exit /b 1 )
+
+echo [3/3] Insertion des donnees de test...
+psql -U %User% -d %DB% -f sql/donnees_mini_test.sql
+if %ERRORLEVEL% neq 0 ( echo ERREUR etape 4 & exit /b 1 )
 
 
 echo Initialisation terminee avec succes.
