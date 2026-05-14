47e4bfc9bc92b9247ab9c0cea9c3a9c24a1952f4
Jemima-Harisoa <jemimarazafimandimby@gmail.com>
2026-05-14 19:12:18 +0300

    fix: Remove obsolete test data and sprint 5 SQL scripts

--- sql/sprint5.sql
+++
@@ -1,8 +0,0 @@
-  ALTER TABLE dossier ADD COLUMN IF NOT EXISTS scan_termine BOOLEAN DEFAULT FALSE;
-  ALTER TABLE dossier ADD COLUMN IF NOT EXISTS date_scan_complete TIMESTAMP;
-  
-INSERT INTO piece_justificative_ref (libelle, id_type_titre) 
-VALUES ('Photo d''identité (webcam)', NULL);
-
-INSERT INTO piece_justificative_ref (libelle, id_type_titre) 
-VALUES ('Signature numérique', NULL);
