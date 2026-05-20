fa39ec0c2d26a83d9be4fd2f3f597222ac830ca5
Jemima-Harisoa <jemimarazafimandimby@gmail.com>
2026-05-20 15:15:41 +0300

    Fix SQL script references in start.bat for data insertion
    - Updated the script to use 'donnees_test.sql' instead of 'donnees_mini_test.sql' for test data insertion.
    - Changed the second data insertion step to execute 'alter_sprint6.sql' instead of 'donnees_test.sql'.

--- start.bat
+++ start.bat
@@ -18,12 +18,12 @@ psql -v ON_ERROR_STOP=1 -U %User% -d postgres -f sql/table.sql
 if %ERRORLEVEL% neq 0 ( echo ERREUR etape 1 & exit /b 1 )
 
 echo [2/3] Insertion des donnees de test...
-psql -v ON_ERROR_STOP=1 -U %User% -d %DB% -f sql/donnees_mini_test.sql
+psql -v ON_ERROR_STOP=1 -U %User% -d %DB% -f sql/donnees_test.sql
 if %ERRORLEVEL% neq 0 ( echo ERREUR etape 4 & exit /b 1 )
 
 
 echo [2/3] Insertion des donnees de test...
-psql -v ON_ERROR_STOP=1 -U %User% -d %DB% -f sql/donnees_test.sql
+psql -v ON_ERROR_STOP=1 -U %User% -d %DB% -f sql\alter_sprint6.sql
 if %ERRORLEVEL% neq 0 ( echo ERREUR etape 4 & exit /b 1 )
 
 echo Initialisation terminee avec succes.
